277c170a81a76b4345ffa1f0a05809cd3227c784
ChristianScarabelli <scachmabol@hotmail.it>
2024-11-29 15:44:40 +0100

    ADD: main sections division

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,16 @@ import Button from './ui/Button/Button.jsx'
 export default function Main() {
     return (
         <>
-
+            <section className='buttons-section'>
+                <div className="container">
+                    bottoni
+                </div>
+            </section>
+            <section className='card-section'>
+                <div className="container">
+                    quì ci va la card
+                </div>
+            </section>
         </>
     )
 }
