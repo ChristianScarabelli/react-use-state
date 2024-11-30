1c53012814c9a5224672c17c98765b63e2ef0466
ChristianScarabelli <scachmabol@hotmail.it>
2024-11-30 22:09:27 +0100

    ADD: language toggle

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,32 +8,39 @@ export default function Main() {
     const [selectedWebLang, setSelectedWebLang] = useState(null) // con il null viene mostrato poi il messaggio di default, perchè di base nessun linguaggio è selezionato
 
     // funzione per aggiornare lo stato del linguaggio selezionato al click
-    const handleSelectedLanguage = (language) => {
-        setSelectedWebLang(language)
+    // SE ESISTE UN LINGUAGGUAGGIO SELEZIONATO (vero), guardo se il suo ID corrisponde a quello del linguaggio cliccato
+    // se è la condizione è vera ritorna null per deselezionare il linguaggio e mostrare lo stato di default
+    // se la condizione è falsa ritorna lang, che è il linguaggio cliccato, per selezionarlo.
+    const toggleSelectedLanguage = (lang) => {
+        setSelectedWebLang(selectedWebLang && selectedWebLang.id === lang.id ? null : lang)
     }
+
     return (
         <>
-            <section className='buttons-section'>
-                <div className="container">
-                    <div className='row'>
-                        {languages.map((lang) => (
-                            <Button
-                                key={lang.id}
-                                title={lang.title}
-                                isActive={selectedWebLang?.id === lang.id}
-                                // lang è l'oggetto corrente dell'array, che viene passato alla funzione che al click aggiorna il linguaggio
-                                onClick={() => handleSelectedLanguage(lang)}
-                            />
-                        ))}
+            <main>
+                <section className='buttons-section'>
+                    <div className="container">
+                        <div className='row'>
+                            {languages.map((lang) => (
+                                <Button
+                                    key={lang.id}
+                                    title={lang.title}
+                                    // isActive={selectedWebLang?.id === lang.id}
+                                    isActive={selectedWebLang && selectedWebLang.id === lang.id}
+                                    // lang è l'oggetto corrente dell'array, che viene passato alla funzione che al click aggiorna il linguaggio
+                                    onClick={() => toggleSelectedLanguage(lang)}
+                                />
+                            ))}
+                        </div>
                     </div>
-                </div>
-            </section>
-            <section className='card-section'>
-                <div className="container">
-                    {/* // inserisco la variabile che contiene il linguaggio, adesso aggiornato dal click*/}
-                    <Card language={selectedWebLang} />
-                </div>
-            </section>
+                </section>
+                <section className='card-section'>
+                    <div className="container">
+                        {/* // inserisco la variabile che contiene il linguaggio, adesso aggiornato dal click*/}
+                        <Card language={selectedWebLang} />
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
